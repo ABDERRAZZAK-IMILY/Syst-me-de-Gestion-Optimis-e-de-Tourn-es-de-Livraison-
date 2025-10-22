cd3cca0798f4ab04ea49504a5641bbbc1c18defa
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-22 12:29:54 +0200

    SDGOTL-15 : Feat : add relation to  Tour  model

--- src/main/java/com/logistics/delivery_optimizer/Model/Tour.java
+++ src/main/java/com/logistics/delivery_optimizer/Model/Tour.java
@@ -3,7 +3,12 @@ package com.logistics.delivery_optimizer.Model;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,6 +31,18 @@ public class Tour {
     @Column(nullable = false)
     private LocalDate TourDate;
 
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouse;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+    
+
+    @OneToMany(mappedBy = "tour",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.LAZY)
+    @OrderColumn(name = "delivery_order") 
+    private List<Delivery> deliveries;
     
 }
