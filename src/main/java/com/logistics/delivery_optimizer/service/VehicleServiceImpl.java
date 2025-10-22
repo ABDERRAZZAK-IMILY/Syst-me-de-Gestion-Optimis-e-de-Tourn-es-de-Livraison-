dd15776fea2c9afc0bca4aab794c1d9cf1e3b7ca
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-22 17:09:21 +0200

    SDGOTL-17 Feat :  Add get Vehicle info method implmentation

--- src/main/java/com/logistics/delivery_optimizer/service/VehicleServiceImpl.java
+++ src/main/java/com/logistics/delivery_optimizer/service/VehicleServiceImpl.java
@@ -6,7 +6,7 @@ import com.logistics.delivery_optimizer.dto.VehicleResponseDTO;
 import com.logistics.delivery_optimizer.mapper.VehicleMapper;
 import com.logistics.delivery_optimizer.repository.VehicleRepository;
 
-public class VehicleServiceImpl extends VehicleService {
+public class VehicleServiceImpl implements VehicleService {
 
 
     private VehicleRepository vehicleRepository;
@@ -17,6 +17,14 @@ public class VehicleServiceImpl extends VehicleService {
         Vehicle vehicle = vehicleMapper.toEntity(vehicleRequestDTO);
         Vehicle save = vehicleRepository.save(vehicle);
         VehicleResponseDTO response = vehicleMapper.tDto(save);
+        return response;
+    }
+
+
+    @Override
+    public VehicleResponseDTO getVehicleInfo(Long vehicleId){
+        Vehicle vehicle = vehicleRepository.findById(vehicleId).orElseThrow(RuntimeException::new);
+        VehicleResponseDTO response = vehicleMapper.tDto(vehicle);
         return response;
     }
 
