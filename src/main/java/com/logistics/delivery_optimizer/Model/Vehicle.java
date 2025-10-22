0df5f0be37d7d758e1f109c24b0fa272cb96d909
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-22 12:28:28 +0200

    SDGOTL-15 : Feat : add relation to  vehicle model

--- src/main/java/com/logistics/delivery_optimizer/Model/Vehicle.java
+++ src/main/java/com/logistics/delivery_optimizer/Model/Vehicle.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import com.logistics.delivery_optimizer.Model.Enums.VehicleType;
-
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -31,6 +33,7 @@ public class Vehicle {
     @Column(nullable = false)
     private String licensePlate;
     
-    
+    @OneToMany(mappedBy = "vehicle", fetch = FetchType.LAZY) 
+    private List<Tour> tours;
     
 }
