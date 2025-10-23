9acf668f126d2384aae43a42047569af1a54bfa1
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-23 12:36:09 +0200

    SDGOTL-20 Refactor : refactor delivery mapper

--- src/main/java/com/logistics/delivery_optimizer/mapper/DeliveryMapper.java
+++ src/main/java/com/logistics/delivery_optimizer/mapper/DeliveryMapper.java
@@ -1,31 +1,38 @@
 package com.logistics.delivery_optimizer.mapper;
 
 import com.logistics.delivery_optimizer.Model.Delivery;
+import com.logistics.delivery_optimizer.Model.Enums.DeliveryStatus;
+import com.logistics.delivery_optimizer.dto.DeliveryRequestDTO;
+import com.logistics.delivery_optimizer.dto.DeliveryResponseDTO;
 
 public class DeliveryMapper {
-    public Delivery toEntity(Delivery dto){
 
+
+    public Delivery toEntity(DeliveryRequestDTO dto) {
+        
         return Delivery.builder()
                 .longitude(dto.getLongitude())
                 .latitude(dto.getLatitude())
                 .weightKg(dto.getWeightKg())
                 .volumeM3(dto.getVolumeM3())
                 .preferredTimeWindow(dto.getPreferredTimeWindow())
-                .status(dto.getStatus())
+                
+                .status(DeliveryStatus.PENDING) 
+                
                 .build();
     }
 
-    public Delivery toDto(Delivery dto){
-        return Delivery.builder()
-                .id(dto.getId())
-                .longitude(dto.getLongitude())
-                .latitude(dto.getLatitude())
-                .weightKg(dto.getWeightKg())
-                .volumeM3(dto.getVolumeM3())
-                .preferredTimeWindow(dto.getPreferredTimeWindow())
-                .status(dto.getStatus())
+
+    public DeliveryResponseDTO toResponseDTO(Delivery entity) {
+        
+        return DeliveryResponseDTO.builder()
+                .id(entity.getId())
+                .longitude(entity.getLongitude())
+                .latitude(entity.getLatitude())
+                .weightKg(entity.getWeightKg())
+                .volumeM3(entity.getVolumeM3())
+                .preferredTimeWindow(entity.getPreferredTimeWindow())
+                .status(entity.getStatus())
                 .build();
     }
-
-    
 }
