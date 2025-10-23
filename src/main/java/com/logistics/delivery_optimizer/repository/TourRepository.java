a2b4889512081e62f702431a65eb56fcb70be496
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-23 16:16:34 +0200

    SDGOTD-31 : Fix : fix name of type in repository

--- src/main/java/com/logistics/delivery_optimizer/repository/TourRepository.java
+++ src/main/java/com/logistics/delivery_optimizer/repository/TourRepository.java
@@ -2,11 +2,7 @@ package com.logistics.delivery_optimizer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.logistics.delivery_optimizer.Model.Delivery;
 import com.logistics.delivery_optimizer.Model.Tour;
-import java.util.List;
 
 public interface TourRepository extends JpaRepository<Tour , Long> {
-    List<Delivery> findByTourId(Long tourId);
-    
 }
