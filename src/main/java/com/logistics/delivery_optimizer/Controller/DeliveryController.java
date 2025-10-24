8f9d8f9d924e1975c1a40a40ece52928e7b08c70
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-24 11:53:46 +0200

    SDGOTL-20 : Feat : add getdelivery method

--- src/main/java/com/logistics/delivery_optimizer/Controller/DeliveryController.java
+++ src/main/java/com/logistics/delivery_optimizer/Controller/DeliveryController.java
@@ -6,8 +6,10 @@ import com.logistics.delivery_optimizer.dto.DeliveryRequestDTO;
 import com.logistics.delivery_optimizer.dto.DeliveryResponseDTO;
 import com.logistics.delivery_optimizer.service.DeliveryService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -23,6 +25,11 @@ public class DeliveryController {
     public DeliveryResponseDTO registerDelivery(@RequestBody DeliveryRequestDTO deliveryRequestDTO){
         return deliveryService.createDelivery(deliveryRequestDTO);
 
+    }
+
+    @GetMapping("/{deliveryId}")
+    public DeliveryResponseDTO getDelivery(@PathVariable Long deliveryId){
+        return deliveryService.getDeliveryById(deliveryId);
     }
     
 
