88deb3a9220c15a1438a839738be5411bb00ecef
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-24 11:47:01 +0200

    SDGOTL-20 : Feat : define delivercontroller bean in appcontext

---
+++ src/main/java/com/logistics/delivery_optimizer/Controller/DeliveryController.java
@@ -0,0 +1,30 @@
+package com.logistics.delivery_optimizer.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.logistics.delivery_optimizer.dto.DeliveryRequestDTO;
+import com.logistics.delivery_optimizer.dto.DeliveryResponseDTO;
+import com.logistics.delivery_optimizer.service.DeliveryService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/deliveries")
+public class DeliveryController {
+    private DeliveryService deliveryService;
+
+    public DeliveryController(DeliveryService deliveryService){
+        this.deliveryService = deliveryService;
+    }
+
+    @PostMapping
+    public DeliveryResponseDTO registerDelivery(@RequestBody DeliveryRequestDTO deliveryRequestDTO){
+        return deliveryService.createDelivery(deliveryRequestDTO);
+
+    }
+    
+
+    
+}
