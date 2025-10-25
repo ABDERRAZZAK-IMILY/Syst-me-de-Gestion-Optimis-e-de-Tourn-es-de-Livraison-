7a7c479c5ed7ccb1b50d3d11effe5de7a37ae78a
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-25 12:54:24 +0200

    SGOTD-26 : Feat : Create warehouseService

--- src/main/java/com/logistics/delivery_optimizer/service/WarehouseService.java
+++ src/main/java/com/logistics/delivery_optimizer/service/WarehouseService.java
@@ -1,11 +1,13 @@
 package com.logistics.delivery_optimizer.service;
 
+import com.logistics.delivery_optimizer.dto.WarehouseRequestDTO;
+import com.logistics.delivery_optimizer.dto.WarehouseResponseDTO;
+import java.util.List;
 
 public interface WarehouseService {
 
+    WarehouseResponseDTO createWarehouse(WarehouseRequestDTO requestDTO);
     WarehouseResponseDTO getWarehouseById(Long id);
-    WarehouseResponseDTO createWarehouse(WarehouseRequestDTO warehouseRequestDTO);
-    WarehouseResponseDTO updateWarehouse(Long id, WarehouseRequestDTO warehouseRequestDTO);
-    void deleteWarehouse(Long id);
+    List<WarehouseResponseDTO> getAllWarehouses();
     
 }
